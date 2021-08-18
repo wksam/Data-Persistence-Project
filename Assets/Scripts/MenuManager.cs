1c6d883c80d74362154512be75a272873c5043e2
Sam <wk.sams@gmail.com>
2021-08-18 15:42:36 -0300

    navigation
    - menu to main
    - main to menu
    - quit game

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
